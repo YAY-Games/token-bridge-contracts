373551f325ec6e9526f91040a425b93592087b31
Алексей Жданов <zhdanoff.alexey@gmail.com>
2021-10-29 03:56:03 +0700

    add etherscan-verify

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,19 @@
 require("@nomiclabs/hardhat-web3");
 require("@nomiclabs/hardhat-truffle5");
+require("@nomiclabs/hardhat-etherscan");
 require('dotenv').config()
 require("solidity-coverage");
+
+const NO_PRIVATE = "0x0000000000000000000000000000000000000000000000000000000000000000";
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || NO_PRIVATE;
+
+const EXPLORER_API_KEY = process.env.EXPLORER_API_KEY || "";
+
+const AVALANCHE_RPC_URL = process.env.AVALANCHE_RPC_URL || "";
+const AVALANCHE_TEST_RPC_URL = process.env.AVALANCHE_TEST_RPC_URL || "";
+const BSC_RPC_URL = process.env.BSC_RPC_URL || "";
+const BSC_TEST_RPC_URL = process.env.BSC_TEST_RPC_URL || "";
 
 const getEnv = env => {
   const value = process.env[env];
@@ -22,10 +34,22 @@ module.exports = {
     hardhat: {
       hardfork: "istanbul"
     },
-    // production: {
-    //   url: getEnv('RPC_URL'),
-    //   accounts: [getEnv('PRIVATE_KEY')]
-    // },
+    bsc: {
+      url: BSC_RPC_URL,
+      accounts: [PRIVATE_KEY]
+    },
+    bscTest: {
+      url: BSC_TEST_RPC_URL,
+      accounts: [PRIVATE_KEY]
+    },
+    avax: {
+      url: AVALANCHE_RPC_URL,
+      accounts: [PRIVATE_KEY]
+    },
+    avaxTest: {
+      url: AVALANCHE_TEST_RPC_URL,
+      accounts: [PRIVATE_KEY]
+    }
   },
   solidity: {
     version: "0.6.12",
@@ -36,5 +60,8 @@ module.exports = {
       },
       evmVersion: "istanbul"
     }
+  },
+  etherscan: {
+    apiKey: EXPLORER_API_KEY
   },
 };
